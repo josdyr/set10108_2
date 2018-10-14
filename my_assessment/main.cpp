408ca8f23d3e75f9c7f8acf2ca4ad62a7c759bbf
Jostein Dyrseth <josdyr@gmail.com>
2018-10-14 17:28:58 +0100

    avg time calculation

--- my_assessment/main.cpp
+++ my_assessment/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <string>
 #include "block_chain.h"
-#include <thread>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 int main()
 {
     block_chain bchain;
 
-    for (uint32_t i = 1; i < 1000u; ++i)
+	auto start = system_clock::now();
+    for (uint32_t i = 1; i < 100u; ++i)
     {
         cout << "Mining block " << i << "..." << endl;
         bchain.add_block(block(i, string("Block ") + to_string(i) + string(" Data")));
     }
+	auto end = system_clock::now();
+	duration<double> diff = end - start;
+	cout << "time of 100 iterations: " << diff.count() << endl;
 
     return 0;
 }
